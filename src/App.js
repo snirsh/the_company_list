0f7a8d4a17739059d82e30c8c6384771a892a40a
Snir Sharristh <s.sharristh@gmail.com>
2021-01-06 23:26:45 +0200

    Added dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 import CompanyTable from './CompanyList';
+import React, { useState } from "react";
+import { ThemeProvider } from "@material-ui/core";
+import { createMuiTheme, makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(2),
+    padding: '8px',
+    height: '100vh',
+    flexGrow: 1,
+  },
+  paper: {
+    width: '100%',
+    height: '100%',
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function App() {
+  const [theme, setTheme] = useState(true);
+  const classes = useStyles();
+  const icon = !theme ? <Brightness7Icon /> : <Brightness3Icon />;
+  const appliedTheme = createMuiTheme(theme ? light : dark);
   return (
-    <div className="App" style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh'}}>
-      <CompanyTable/>
-    </div>
+    <ThemeProvider theme={appliedTheme}>
+      <Paper>
+          <div className={classes.root}>
+          <IconButton
+            edge="end"
+            color="inherit"
+            aria-label="mode"
+            onClick={() => setTheme(!theme)}
+          >
+            {icon}
+          </IconButton>
+          <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '20vh'}}>
+            <CompanyTable/>
+          </div>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export const light = {
+  palette: {
+    type: "light"
+  }
+};
+export const dark = {
+  palette: {
+    type: "dark"
+  }
+};
